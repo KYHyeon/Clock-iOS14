3eac81be0baa57cb9f6b1837766788b59acd74cc
KYHyeon <run190822@gmail.com>
2020-08-30 18:42:55 +0900

    Initial Commit

---
+++ Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by 현기엽 on 2020/08/30.
+//  Copyright © 2020 현기엽. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
