dc9a2e147d78421abe0cc06d0637e114be5bda53
KYHyeon <run190822@gmail.com>
2020-09-02 18:28:09 +0900

    [WorldTime] feat: Add append method in viewModel

--- Clock/WorldTime/WorldTime.swift
+++ Clock/WorldTime/WorldTime.swift
@@ -11,20 +11,24 @@ import Combine
 
 class WorldTime: ObservableObject {
     
-    @Published var cities = [
+    @Published private(set) var cities = [
         City(diffHour: 0, name: "서울"),
         City(diffHour: -14, name: "뉴올리언즈"),
         City(diffHour: -6, name: "나이로비")
     ]
     
-    
     private var timerCancellable: AnyCancellable?
     
+    //MARK - Intent
     func delete(_ indexSet: IndexSet) {
         cities.remove(atOffsets: indexSet)
     }
     
     func move(source: IndexSet, destination: Int) {
         cities.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func append(city: City) {
+        cities.append(city)
     }
 }
