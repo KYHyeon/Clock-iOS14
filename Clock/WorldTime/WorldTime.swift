16bb5813ff6bc1d2270de641994cda972a733f3e
KYHyeon <run190822@gmail.com>
2020-09-04 20:33:09 +0900

    [WorldTime] Modify allcities mock data

--- Clock/WorldTime/WorldTime.swift
+++ Clock/WorldTime/WorldTime.swift
@@ -19,15 +19,15 @@ class WorldTime: ObservableObject {
     
     private var timerCancellable: AnyCancellable?
     
-    var allCities = [
-        City(diffHour: 0, name: "서울"),
-        City(diffHour: 0, name: "대구"),
-        City(diffHour: 0, name: "광주"),
-        City(diffHour: 0, name: "의정부"),
-        City(diffHour: 0, name: "울산"),
-        City(diffHour: -14, name: "뉴올리언즈"),
-        City(diffHour: -6, name: "나이로비")
-    ]
+    var allCities: [City] = TimeZone.knownTimeZoneIdentifiers.compactMap { identifier in
+        guard let timezone = TimeZone(identifier: identifier) else { return nil }
+        let diffHour = (TimeZone.current.secondsFromGMT() - timezone.secondsFromGMT()) / 3600
+        var name: String = timezone.localizedName(
+            for: NSTimeZone.NameStyle.shortGeneric,
+            locale: Locale(identifier: "ko_KR")
+        ) ?? ""
+        return City(diffHour: diffHour, name: name)
+    }
     
     // MARK: - Intent
     func delete(_ indexSet: IndexSet) {
